17b981d3361de4a04b12d6a1bcbf7a4bbbc7a7f1
Sudarshan <sudarshan85@gmail.com>
2021-11-01 12:51:46 -0400

    Added README

---
+++ src/README.md
@@ -0,0 +1,18 @@
+`aggregate_files.py` does the following:
+
+  1. Aggregates language files across different documents into a single file
+
+  2. Sets up the data to be using via HuggingFace's datasets library and hub with training and testing splits
+
+This script needs to be run on a specific directory containing folders corresponding to each
+document. These folders should contain text files associated with each language that is being processed.
+There can be multiple languages per folder.
+
+Multiple language files result in multiple datasets. For example, processing files inside english-macedonian-shqip
+will result in:
+
+  1. english.txt, macedonian.txt, shqip.txt in that folder
+
+  2. en-ma and en-sh folders in datasets/huggingface_hub
+
+  3. within each we will have train and test files for machine translation
--- src/aggregate_files.py
+++ src/aggregate_files.py
@@ -1,10 +1,36 @@
 #!/usr/bin/env python
 
-import argparse
+import textwrap
+from argparse import ArgumentParser, HelpFormatter, Namespace
 from pathlib import Path
 
-def parse_args() -> argparse.Namespace:
-  parser = argparse.ArgumentParser(description="Script to aggregate language files across different documents into a single file", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+script_description = """
+This script does the following:
+
+  1. Aggregates language files across different documents into a single file
+  2. Sets up the data to be using via HuggingFace's datasets library and hub with training and testing splits
+
+This script needs to be run on a specific directory containing folders corresponding to each
+document. These folders should contain text files associated with each language that is being processed.
+There can be multiple languages per folder.
+
+Multiple language files result in multiple datasets. For example, processing files inside english-macedonian-shqip
+will result in:
+
+  1. english.txt, macedonian.txt, shqip.txt in that folder
+  2. en-ma and en-sh folders in datasets/huggingface_hub
+  3. within each we will have train and test files for machine translation
+"""
+
+class RawFormatter(HelpFormatter):
+  def _fill_text(self, text: str, width: int, indent: str) -> str:
+      return '\n'.join([textwrap.fill(line, width) for line in textwrap.indent(textwrap.dedent(text), indent).splitlines()])
+    
+
+languages = ['english', 'macedonian', 'shqip']
+
+def parse_args() -> Namespace:
+  parser = ArgumentParser(description=script_description, formatter_class=RawFormatter)
   parser.add_argument('-d', '--dir', type=str, help='Directory containing sub-directories corresponding to different documents', required=True)
   return parser.parse_args()
 
